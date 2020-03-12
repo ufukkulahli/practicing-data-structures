1cfc1ce37be0e2f3285ff4640fcec05498ec11d5
ufukkulahli <ufukkulahli@gmail.com>
2020-03-12 22:36:33 +0300

    Implement 'Add' functionality into 'ArrayList'.

--- data-structures/lists/ArrayList.cs
+++ data-structures/lists/ArrayList.cs
@@ -1,8 +1,27 @@
+using System;
+
 namespace practicing_data_structures.data_structures.lists
 {
-    public class ArrayList<T>
+  public class ArrayList<T>
+  {
+    private int size  = 0;
+    private T[] items = new T[0];
+
+    public int Count()    => size;
+    public int Capacity() => items.Length;
+
+    public void Add(T item)
     {
-      private int size;
-      public int Count() => this.size;
+      if(HasNotEnoughCapacity())
+      {
+        ExpandCapacity();
+      }
+      items[size++] = item;
     }
+
+    private bool HasNotEnoughCapacity()  => size == items.Length;
+    private void ExpandCapacity()        => Array.Resize<T>(ref items, NewCapacitySize());
+    private int NewCapacitySize()        => NoItems() ? 1 : items.Length * 2;
+    private bool NoItems()               => items.Length == 0;
+  }
 }
--- tests/ArrayListTest.cs
+++ tests/ArrayListTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using practicing_data_structures.data_structures.lists;
 using Xunit;
 
@@ -10,14 +11,15 @@ namespace practicing_data_structures
     public void Test()
     {
       // Arrange
-      var al = new ArrayList<string>();
+      var words = new ArrayList<string>();
 
-      // Act
-      var count = al.Count();
-      Assert.Equal(0, count);
+      // Act && Assert
+      Assert.Equal(0, words.Count());
+      Assert.Equal(0, words.Capacity());
 
-
-      // Assert
+      words.Add("hello");
+      Assert.Equal(1, words.Count());
+      Assert.Equal(1, words.Capacity());
     }
   }
 }
