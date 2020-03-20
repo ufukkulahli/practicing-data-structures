84467d72a2cc3e8c8ea94a529c9f101256a8f2ba
ufukkulahli <ufukkulahli@gmail.com>
2020-03-20 22:19:01 +0300

    Start implementing 'Stack' data structure and its test.

---
+++ data-structures/lists/Stack.cs
@@ -0,0 +1,7 @@
+namespace practicing_data_structures.data_structures.lists
+{
+  public sealed class Stack<T>
+  {
+    
+  }
+}
---
+++ tests/StackTests.cs
@@ -0,0 +1,15 @@
+using practicing_data_structures.data_structures.lists;
+using Xunit;
+
+namespace practicing_data_structures.tests
+{
+  public class StackTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange
+      var words = new Stack<string>();
+    }
+  }
+}
