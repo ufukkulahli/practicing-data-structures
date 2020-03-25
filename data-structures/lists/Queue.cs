a5e6416566144b48805a5393bfb3066f28527223
ufukkulahli <ufukkulahli@gmail.com>
2020-03-25 19:35:34 +0300

    Start 'Queue' data structure implementation.

---
+++ data-structures/lists/Queue.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace practicing_data_structures.data_structures.lists
+{
+  public sealed class Queue<T>
+  {
+  }
+}
---
+++ tests/lists/QueueTests.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Linq;
+using Xunit;
+using practicing_data_structures.data_structures.lists;
+
+namespace practicing_data_structures.tests
+{
+  public class QueueTests
+  {
+    [Fact]
+    public void Test()
+    {
+      // Arrange
+      var words = new Queue<string>();
+
+      // Act && Assert
+
+      //
+    }
+  }
+}
