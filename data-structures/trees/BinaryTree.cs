0be6d79484fea38d8e2fdd151c189dac99b78dbf
ufukkulahli <ufukkulahli@gmail.com>
2020-04-03 18:56:54 +0300

    Add 'Node' class for 'BinaryTree'.

--- data-structures/trees/BinaryTree.cs
+++ data-structures/trees/BinaryTree.cs
@@ -1,5 +1,12 @@
 namespace practicing_data_structures.data_structures.trees
 {
+  public sealed class Node
+  {
+    public readonly int value;
+    public Node Left, Right;
+    public Node(int v) => value = v;
+  }
+
   public sealed class BinaryTree
   {
   }
