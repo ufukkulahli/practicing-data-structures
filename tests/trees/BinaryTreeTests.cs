3ac476020d64096b415dff55d5b77a7223db7c21
ufukkulahli <ufukkulahli@gmail.com>
2020-04-05 23:03:15 +0300

    Add asserts for 'BinaryTree' test.

--- tests/trees/BinaryTreeTests.cs
+++ tests/trees/BinaryTreeTests.cs
@@ -8,12 +8,20 @@ namespace practicing_data_structures.tests.trees
     [Fact]
     public void Test()
     {
-      
       // Arrange
       var bt = new BinaryTree<int>(BuildBinaryTreeFromNodes());
 
       // Act && Assert
       Assert.Equal(1, bt.root.value);
+
+      Assert.Equal(2, bt.root.Left.value);
+      Assert.Equal(3, bt.root.Right.value);
+
+      Assert.Equal(4, bt.root.Left.Left.value);
+      Assert.Equal(5, bt.root.Left.Right.value);
+
+      Assert.Equal(8, bt.root.Left.Left.Left.value);
+      Assert.Equal(9, bt.root.Left.Left.Right.value);
     }
 
     public Node<int> BuildBinaryTreeFromNodes()
@@ -29,12 +37,12 @@ namespace practicing_data_structures.tests.trees
           tree.Left.Left = new Node<int>(4);
           tree.Left.Right = new Node<int>(5);
       
-          tree.Right.Left = new Node<int>(4);
-          tree.Right.Right = new Node<int>(5);
+          tree.Right.Left = new Node<int>(6);
+          tree.Right.Right = new Node<int>(7);
       
             // Depth: 4
-            tree.Left.Left.Left = new Node<int>(4);
-            tree.Left.Left.Right = new Node<int>(5);
+            tree.Left.Left.Left = new Node<int>(8);
+            tree.Left.Left.Right = new Node<int>(9);
 
       return tree;
     }
