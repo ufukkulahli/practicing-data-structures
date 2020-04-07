83f6485f7765e97cb32da7e0c7ed0350d447cbe6
ufukkulahli <ufukkulahli@gmail.com>
2020-04-07 22:42:15 +0300

    Add 'Node' type for 'SinglyLinkedList'. Convert them to a generic type.

--- data-structures/lists/SinglyLinkedList.cs
+++ data-structures/lists/SinglyLinkedList.cs
@@ -1,6 +1,14 @@
 namespace practicing_data_structures.data_structures.lists
 {
-  public class SinglyLinkedList
+  public sealed class Node<T>
   {
+    T value;
+    Node<T> Next;
+    public Node(T value) => this.value = value;
+  }
+
+  public sealed class SinglyLinkedList<T>
+  {
+    Node<T> head;
   }
 }
