0672cfdd5263b4bce38fb73b5113d1b01f3002b2
ufukkulahli <ufukkulahli@gmail.com>
2020-04-27 23:20:25 +0300

    Fill in 'Node' class.

--- data-structures/lists/doubly/Node.cs
+++ data-structures/lists/doubly/Node.cs
@@ -2,6 +2,16 @@ namespace practicing_data_structures.data_structures.lists.doubly
 {
   public sealed class Node<T>
   {
-    public T Value { get; }
+    public T Value { get; private set; }
+    public Node<T> Previous { get; private set; }
+    public Node<T> Next { get; private set; }
+
+    public Node(T value) : this (value, null, null) {}
+    public Node(T value, Node<T> previous, Node<T> next)
+    {
+      Value    = value;
+      Previous = previous;
+      Next     = next;
+    }
   }
 }
