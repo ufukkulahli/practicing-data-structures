a29c73e8c3105279727d4c5ed6b59cc0a57630e8
ufukkulahli <ufukkulahli@gmail.com>
2020-12-03 23:11:22 +0300

    Fix 'InsertRightSideOfTree' test.

--- tests/trees/AVLTreeTests.cs
+++ tests/trees/AVLTreeTests.cs
@@ -231,13 +231,10 @@ namespace practicing_data_structures.tests.trees
       tree.Insert(6);
 
       // Assert
-      Assert.Equal(4, tree.Root.Value);
-      Assert.Equal(5, tree.Root.Right.Value);
-      Assert.Equal(6, tree.Root.Right.Right.Value);
-      Assert.Null(tree.Root.Left);
-      Assert.Null(tree.Root.Right.Left);
-      Assert.Null(tree.Root.Right.Right.Left);
-      Assert.Equal(2, tree.Height());
+      Assert.Equal(5, tree.Root.Value);
+      Assert.Equal(4, tree.Root.Left.Value);
+      Assert.Equal(6, tree.Root.Right.Value);
+      Assert.Equal(1, tree.Height());
       Assert.Equal(3, tree.Root.Count);
     }
 
