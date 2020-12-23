f30d6c6a642fa3d53b84ef50d5bd50cebff497ab
ufukkulahli <ufukkulahli@gmail.com>
2020-12-23 22:08:41 +0300

    Add test case for 'deleting & balancing' nodes.

--- tests/trees/AVLTreeDeleteThenBalanceTests.cs
+++ tests/trees/AVLTreeDeleteThenBalanceTests.cs
@@ -37,5 +37,36 @@ namespace practicing_data_structures.tests.trees
       Assert.Equal(18, tree.Root.Right.Right.Value);
     }
 
+    [Fact]
+    public void Delete_When_NodeIsLeaf_And_RightChild()
+    {
+      // Arrange
+      var tree = new AVLTree<int>();
+
+      tree.Insert(18); 
+      tree.Insert(16); 
+      tree.Insert(14); 
+      tree.Insert(12); 
+      tree.Insert(10); 
+
+      Assert.Equal(16, tree.Root.Value);
+      Assert.Equal(12, tree.Root.Left.Value);
+      Assert.Equal(18, tree.Root.Right.Value);
+      Assert.Equal(10, tree.Root.Left.Left.Value);
+      Assert.Equal(14, tree.Root.Left.Right.Value); 
+
+      // Act
+      tree.Delete(18);
+
+      // Assert
+      Assert.Equal(16, tree.Root.Value);
+      // Left
+      Assert.Equal(12, tree.Root.Left.Value);
+      Assert.Equal(10, tree.Root.Left.Left.Value);
+      Assert.Equal(14, tree.Root.Left.Right.Value); 
+      // Right
+      Assert.Equal(null, tree.Root.Right);
+    }
+
   }
 }
