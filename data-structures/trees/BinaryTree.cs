f27d60000702f32680b9aee2ea46a73309d936d5
ufukkulahli <ufukkulahli@gmail.com>
2021-01-03 22:24:52 +0300

    Make 'T' of BinaryTree & Node 'IComparable'.

--- data-structures/trees/BinaryTree.cs
+++ data-structures/trees/BinaryTree.cs
@@ -1,13 +1,15 @@
+using System;
+
 namespace practicing_data_structures.data_structures.trees
 {
-  public sealed class Node<T>
+  public sealed class Node<T> where T : IComparable
   {
     public readonly T value;
     public Node<T> Left, Right;
     public Node(T v) => value = v;
   }
 
-  public sealed class BinaryTree<T>
+  public sealed class BinaryTree<T> where T : IComparable
   {
     public readonly Node<T> root;
     public BinaryTree(Node<T> r) => root = r;
