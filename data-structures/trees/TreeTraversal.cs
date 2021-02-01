eed8194ef0373f8290381bfbd2b2aab46e12420a
ufukkulahli <ufukkulahli@gmail.com>
2021-02-01 22:28:23 +0300

    Complete 'TreeTraversal.Inorder' functionality.

--- data-structures/trees/TreeTraversal.cs
+++ data-structures/trees/TreeTraversal.cs
@@ -1,12 +1,25 @@
 using System;
+using System.Text;
 
 namespace practicing_data_structures.data_structures.trees
 {
   public sealed class TreeTraversal<T> where T : IComparable
   {
-    public string Inorder(BinaryTreeNode<T> node)
+    private StringBuilder sb;
+    public TreeTraversal(StringBuilder sb) => this.sb = sb;
+
+    public void Inorder(BinaryTreeNode<T> node)
     {
-      throw new System.NotImplementedException();
+      if(node==null)
+      {
+        return;
+      }
+
+      Inorder(node.Left);
+
+      this.sb.Append(node.value + "->");
+
+      Inorder(node.Right);
     }
   }
 }
--- tests/trees/TreeTraversalTests.cs
+++ tests/trees/TreeTraversalTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using practicing_data_structures.data_structures.trees;
 using Xunit;
 
@@ -8,7 +9,25 @@ namespace practicing_data_structures.tests.trees
     [Fact]
     public void Test()
     {
-      Assert.Throws<System.NotImplementedException>( () => new TreeTraversal<int>().Inorder(null) );
+      // Arrange
+      var bt = new BinaryTree<int>(null);
+      bt.Insert(5);
+      bt.Insert(5, 10);
+      bt.Insert(5, 12);
+      
+      bt.Insert(10, 20);
+      bt.Insert(10, 22);
+      
+      bt.Insert(12, 30);
+      bt.Insert(12, 32);
+
+      var sb = new StringBuilder();
+
+      // Act
+      new TreeTraversal<int>(sb).Inorder(bt.root);
+
+      // Assert
+      Assert.Equal("20->10->22->5->30->12->32", sb.ToString().TrimEnd('>').TrimEnd('-'));
     }
   }
 }
