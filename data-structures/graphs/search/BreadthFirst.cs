b3b55d9cf0a92ea79ceca01169617cde572edfb6
ufukkulahli <ufukkulahli@gmail.com>
2021-02-05 23:10:20 +0300

    Start 'BreadthFirst.Find' functionality.

---
+++ data-structures/graphs/search/BreadthFirst.cs
@@ -0,0 +1,7 @@
+namespace practicing_data_structures.data_structures.graphs.search
+{
+  public sealed class BreadthFirst
+  {
+    public bool Find() => throw new System.NotImplementedException();
+  }
+}
---
+++ tests/graphs/search/BreadthFirstTests.cs
@@ -0,0 +1,14 @@
+using practicing_data_structures.data_structures.graphs.search;
+using Xunit;
+
+namespace practicing_data_structures.tests.graphs.search
+{
+  public class BreadthFirstTests
+  {
+    [Fact]
+    public void Test()
+    {
+      Assert.Throws<System.NotImplementedException>( () => new BreadthFirst().Find() );
+    }
+  }
+}
