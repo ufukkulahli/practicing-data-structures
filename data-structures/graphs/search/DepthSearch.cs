f6b4fc547d01fe188405cfa6ca5d29dedca9007a
ufukkulahli <ufukkulahli@gmail.com>
2021-02-11 21:49:59 +0300

    Start 'DepthFirst.Find' functionality.

---
+++ data-structures/graphs/search/DepthSearch.cs
@@ -0,0 +1,7 @@
+namespace practicing_data_structures.data_structures.graphs.search
+{
+  public sealed class DepthSearch
+  {
+    public bool Find() => throw new System.NotImplementedException();
+  }
+}
