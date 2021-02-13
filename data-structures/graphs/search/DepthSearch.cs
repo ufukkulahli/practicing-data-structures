c681901fba67c8c7ccd5ec1c76476d43884c39ba
ufukkulahli <ufukkulahli@gmail.com>
2021-02-13 17:22:50 +0300

    Continue 'DepthFirst.Find' functionality.

--- data-structures/graphs/search/DepthSearch.cs
+++ data-structures/graphs/search/DepthSearch.cs
@@ -1,7 +1,15 @@
 namespace practicing_data_structures.data_structures.graphs.search
 {
-  public sealed class DepthSearch
+  public sealed class DepthSearch<T>
   {
-    public bool Find() => throw new System.NotImplementedException();
+    public bool Find(Vertex<T> startVertex, T searchedVertexValue)
+    {
+      if(startVertex.Value.Equals(searchedVertexValue))
+      {
+        return true;
+      }
+
+      throw new System.NotImplementedException();
+    }
   }
 }
--- tests/graphs/search/DepthFirstTests.cs
+++ tests/graphs/search/DepthFirstTests.cs
@@ -1,3 +1,4 @@
+using practicing_data_structures.data_structures.graphs;
 using practicing_data_structures.data_structures.graphs.search;
 using Xunit;
 
@@ -5,10 +6,23 @@ namespace practicing_data_structures.tests.graphs.search
 {
   public class DepthFirstTests
   {
+
     [Fact]
-    public void Test()
+    public void SearchedVertexValueIsTheFirstOne()
     {
-      Assert.Throws<System.NotImplementedException>( () => new DepthSearch().Find() );
+      // Arrange
+      var graph = new Graph<char>();
+
+      graph.AddVertex('A');
+      graph.AddVertex('B');
+
+      //graph.AddEdge('A', 'B');
+
+      var depthFirst = new DepthSearch<char>();
+
+      // Act & Assert
+      Assert.True(depthFirst.Find(graph.FirstVertexOrNull, 'A'));
     }
+
   }
 }
