2cec93e0dc4456b65eaf817510fe8a14a21046ba
ufukkulahli <ufukkulahli@gmail.com>
2021-02-17 21:48:28 +0300

    Start 'BFSGraph.AddEdge' functionality.

---
+++ data-structures/graphs/BFSGraph.cs
@@ -0,0 +1,11 @@
+namespace practicing_data_structures.data_structures.graphs
+{
+  public sealed class BFSGraph
+  {
+    public void AddEdge()
+    {
+      throw new System.NotImplementedException();
+    }    
+  }
+
+}
---
+++ tests/graphs/BFSGraphTests.cs
@@ -0,0 +1,14 @@
+using practicing_data_structures.data_structures.graphs;
+using Xunit;
+
+namespace practicing_data_structures.tests.graphs
+{
+  public class BFSGraphTests
+  {
+    [Fact]
+    public void Test()
+    {
+      Assert.Throws<System.NotImplementedException>( () => new BFSGraph().AddEdge() );
+    }
+  }
+}
