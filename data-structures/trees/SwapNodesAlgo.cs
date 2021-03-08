c7a131701bbd56b661bcde55c88379b95479a2b2
ufukkulahli <ufukkulahli@gmail.com>
2021-03-08 23:05:44 +0300

    Start 'Swap nodes algorithm'.

---
+++ data-structures/trees/SwapNodesAlgo.cs
@@ -0,0 +1,10 @@
+namespace practicing_data_structures.data_structures.trees
+{
+  public sealed class SwapNodesAlgo
+  {
+    public void Swap()
+    {
+      throw new System.NotImplementedException();
+    }
+  }
+}
---
+++ tests/trees/SwapNodesAlgoTests.cs
@@ -0,0 +1,15 @@
+using practicing_data_structures.data_structures.trees;
+using Xunit;
+
+namespace practicing_data_structures.tests.trees
+{
+  public class SwapNodesAlgoTests
+  {
+    [Fact]
+    public void Test()
+    {
+      Assert.Throws<System.NotImplementedException>( () => new SwapNodesAlgo().Swap() );
+    }
+  }
+
+}
