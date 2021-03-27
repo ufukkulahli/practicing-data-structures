8cebe0d0f54a113e60041066d46609e1c7f0f468
ufukkulahli <ufukkulahli@gmail.com>
2021-03-27 22:36:00 +0300

    Continue the new 'Swap nodes algorithm', add test.

--- data-structures/trees/SwapNodesAlgo2.cs
+++ data-structures/trees/SwapNodesAlgo2.cs
@@ -10,9 +10,23 @@ namespace practicing_data_structures.data_structures.trees
 
     public void Swap(int[][] indexes, int node, int targetDepth, int depth, int[][] result)
     {
-      if(node==-1)
+      if (node == -1)
       {
-        return;
+          return;
+      }
+
+      SwapNodes(indexes, node, targetDepth, depth);
+
+      throw new System.NotImplementedException();
+    }
+
+    public void SwapNodes(int[][] indexes, int node, int targetDepth, int depth)
+    {
+      if (depth % targetDepth == 0)
+      {
+        var tempLeftIndex = indexes[node][0];
+        indexes[node][0]  = indexes[node][1];
+        indexes[node][1]  = tempLeftIndex;
       }
     }
 
--- tests/trees/SwapNodesAlgo2Tests.cs
+++ tests/trees/SwapNodesAlgo2Tests.cs
@@ -13,6 +13,28 @@ namespace practicing_data_structures.tests.trees
     }
 
     [Fact]
+    public void SwapNodeTest()
+    {
+      // Arrange
+      var indexes     = new int[1][];
+      indexes[0]      = new int[2];
+      indexes[0][0]   = 2;
+      indexes[0][1]   = 3;
+
+      var node        = 0;
+      var targetDepth = 1;
+      var depth       = 1;
+      var swapNodesAlgo2 = new SwapNodesAlgo2();
+
+      // Act
+      swapNodesAlgo2.SwapNodes(indexes, node, targetDepth, depth);
+
+      // Assert
+      Assert.Equal(3, indexes[0][0]);
+      Assert.Equal(2, indexes[0][1]);
+    }
+
+    [Fact]
     public void SwapTest2()
     {
       // Arrange
