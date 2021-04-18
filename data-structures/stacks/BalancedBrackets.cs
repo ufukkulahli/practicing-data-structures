13d96ef18e7bc0a3f467af996dfe2b6d442c8ca2
ufukkulahli <ufukkulahli@gmail.com>
2021-04-18 22:16:08 +0300

    Continue 'BalancedBrackets'.

--- data-structures/stacks/BalancedBrackets.cs
+++ data-structures/stacks/BalancedBrackets.cs
@@ -6,5 +6,26 @@ namespace practicing_data_structures.data_structures.stacks
     {
       throw new System.NotImplementedException();
     }
+
+    public bool IsBalanced(char currBracketFromHead, char currBracketFromTail)
+    {
+      if(currBracketFromHead=='{')
+      {
+        return currBracketFromTail=='}';
+      }
+
+      if(currBracketFromHead=='[')
+      {
+        return currBracketFromTail==']';
+      }
+
+      if(currBracketFromHead=='(')
+      {
+        return currBracketFromTail==')';
+      }
+
+      return false;
+    }
+
   }
 }
--- tests/stacks/BalancedBracketsTests.cs
+++ tests/stacks/BalancedBracketsTests.cs
@@ -10,5 +10,62 @@ namespace practicing_data_structures.tests.stacks
     {
       Assert.Throws<System.NotImplementedException>( () => new BalancedBrackets().IsBalanced(null) );
     }
+
+    [Fact]
+    public void BracketsBalanced1()
+    {
+      // Arrange
+      var bracketOpen = '{';
+      var bracketClose = '}';
+
+      // Act
+      var isBalanced = new BalancedBrackets().IsBalanced(bracketOpen, bracketClose);
+
+      // Assert
+      Assert.True(isBalanced);
+    }
+
+    [Fact]
+    public void BracketsBalanced2()
+    {
+      // Arrange
+      var bracketOpen = '[';
+      var bracketClose = ']';
+
+      // Act
+      var isBalanced = new BalancedBrackets().IsBalanced(bracketOpen, bracketClose);
+
+      // Assert
+      Assert.True(isBalanced);
+    }
+
+    [Fact]
+    public void BracketsBalanced3()
+    {
+      // Arrange
+      var bracketOpen = '(';
+      var bracketClose = ')';
+
+      // Act
+      var isBalanced = new BalancedBrackets().IsBalanced(bracketOpen, bracketClose);
+
+      // Assert
+      Assert.True(isBalanced);
+    }
+
+    [Fact]
+    public void BracketsNotBalanced()
+    {
+      // Arrange
+      var bracketOpen = '(';
+      var bracketClose = ']';
+
+      // Act
+      var isBalanced = new BalancedBrackets().IsBalanced(bracketOpen, bracketClose);
+
+      // Assert
+      Assert.False(isBalanced);
+    }
+
   }
 }
